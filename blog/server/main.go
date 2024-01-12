bcf5a2214f5aef0a46af369a70b00c9549d90558
Felipe <flpc.dev@gmail.com>
2024-01-12 16:42:26 -0300

    feat: init blog grpc and implement create blog in serverside

---
+++ blog/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/flpcastro/grpc-go/blog/proto"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+)
+
+var collection *mongo.Collection
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.BlogServiceServer
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("blogdb").Collection("blog")
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on: %s\n", addr)
+
+	s := grpc.NewServer()
+	pb.RegisterBlogServiceServer(s, &Server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
